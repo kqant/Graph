e78c98bf88055405b7fd69d2229083f1eef8d9b8
Maxim <deadly536@gmail.com>
2021-05-05 20:58:16 +0500

    init commit v3.01

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-<<<<<<< Updated upstream
-
-import sys
-
-from PyQt6.QtWidgets import QApplication
-
-from GraphUI import GraphUI
-from GraphModel import GraphModel
-from GraphCtrl import GraphCtrl
-
-
-def main():
-    app = QApplication(sys.argv)
-
-    #init view
-    view = GraphUI()
-    view.show()
-
-    #init model
-    model = GraphModel()
-
-    #init control
-    GraphCtrl(model, view)
-
-    sys.exit(app.exec())
-
-
-if __name__ == "__main__":
-    main()
-
-=======
 from PyQt5.QtWidgets import QApplication
 
 import sys
@@ -56,4 +25,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
->>>>>>> Stashed changes
