e504567817508118a600a8e9859910f360355c11
kqant <kqanto@gmail.com>
2021-05-06 15:45:57 +0500

    added converter matrix to list

--- graph.py
+++ graph.py
@@ -6,6 +6,17 @@ class Graph():
     def __init__(self):
         pass
 
+    def convert_matrix_to_list(self, matrix):
+        G = {}
+        for a in range(len(matrix)):
+            for b in range(len(matrix)):
+                if matrix[a][b]:
+                    if a + 1 not in G:
+                        G[a + 1] = {b + 1: matrix[a][b]}
+                    else:
+                        G[a + 1][b + 1] = matrix[a][b]
+        return G
+
     def getGraphX(self):
         pass
 
