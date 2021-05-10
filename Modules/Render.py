e297160051c0142444a62ec467378e58e0987a41
smaylninja <74140762+smaylninja@users.noreply.github.com>
2021-05-10 12:16:01 +0500

    add weighted (full)

--- Modules/Render.py
+++ Modules/Render.py
@@ -40,10 +40,16 @@ def drawMinPath(view, adj, is_dir, weighted, path):
     
     for i in adj:
         for j in adj[i]:
-            G.add_edge(i, j, weight=adj[i][j], color='#750000')
+            if (weighted):
+                G.add_edge(i, j, weight=adj[i][j], color='#750000')
+            else:
+                G.add_edge(i, j, color='#750000')
             for k in range(len(path) - 1):
                 if ((path[k] == i) and (path[k + 1] == j)) or ((path[k] == j) and (path[k+1] == i)):
-                    G.add_edge(i, j, weight=adj[i][j], color='blue')
+                    if (weighted):
+                        G.add_edge(i, j, weight=adj[i][j], color='blue')
+                    else:
+                        G.add_edge(i, j, color='blue')
                     break
 
     pos = nx.kamada_kawai_layout(G)
@@ -70,9 +76,14 @@ def drawColoring(view, adj, is_dir, weighted, colors):
             col_converted.append((colors[i][0] / 255, colors[i][1] / 255, colors[i][2] / 255))
         return col_converted
     
-    for i in adj:
-        for j in adj[i]:
-            G.add_edge(i, j, weight=adj[i][j], color='#750000')
+    if (weighted):
+        for i in adj:
+            for j in adj[i]:
+                G.add_edge(i, j, weight=adj[i][j])
+    else:
+        for i in adj:
+            for j in adj[i]:
+                G.add_edge(i, j)
     
     pos = nx.kamada_kawai_layout(G)
     
