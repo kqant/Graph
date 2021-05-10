a85693acc2e32ce0644309cefe25e9020fc3ed60
smaylninja <74140762+smaylninja@users.noreply.github.com>
2021-05-10 10:34:42 +0500

    update minpath

--- Modules/Render.py
+++ Modules/Render.py
@@ -27,7 +27,29 @@ def drawDefault(view, adj, is_dir, weighted):
 
 
 def drawMinPath(view, path):
-    pass
+    view.figure.clf()
+    
+    if (is_dir):
+        G = nx.DiGraph()
+    else:
+        G = nx.Graph()
+    
+    for i in adj:
+        for j in adj[i]:
+            G.add_edge(i, j, weight=adj[i][j], color='#750000')
+            for k in range(len(path) - 1):
+                if ((path[k] == i) and (path[k + 1] == j)) or ((path[k] == j) and (path[k+1] == i)):
+                    G.add_edge(i, j, weight=adj[i][j], color='blue')
+                    break
+
+    pos = nx.kamada_kawai_layout(G)
+    
+    nx.draw(G, pos=pos, with_labels=True, node_color='#003473', font_color='white', font_weight='bold', alpha=0.9)
+    nx.draw_networkx_edge_labels(G, pos=pos, font_color='black', font_weight=700,
+                             edge_labels=nx.get_edge_attributes(G, 'weight'))
+    nx.draw_networkx_edges(G, pos=pos, width=2, edge_color=nx.get_edge_attributes(G,'color').values())
+    
+    view.canvas.draw()
 
 
 def drawColoring(view, colors):
