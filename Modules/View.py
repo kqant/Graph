fa72fdc3e8ccf765df90f959f1bdfefe4275bc0a
Alexander <37180898+MrSago@users.noreply.github.com>
2021-05-15 13:20:20 +0300

    refact2

--- Modules/View.py
+++ Modules/View.py
@@ -10,6 +10,7 @@ from os import path, getcwd, utime
 
 iconPath = path.join(getcwd(), "Icons/GraphDrawer.svg")
 
+
 class GraphUI(QMainWindow):
     def __init__(self, parent = None):
         super(GraphUI, self).__init__(parent)
@@ -26,18 +27,19 @@ class GraphUI(QMainWindow):
         self.graphwidget = QWidget(self)
         self.generalLayout = QVBoxLayout(self.graphwidget)
 
-        self.createButtons()
-        self.createCanvas()
-        self.createMinPathInput()
-        self.createAlgoOutput()
+        self._createButtons()
+        self._createCanvas()
+        self._createMinPathInput()
+        self._createAlgoOutput()
 
         QMetaObject.connectSlotsByName(self)
 
-        self.setObjectsNames()
-        self.setGeometry()
+        self._setObjectsNames()
+        self._setGeometry()
         self.figure.clf()
 
-    def createButtons(self):
+
+    def _createButtons(self):
         self.buttons = {}
         buttons = {
             "Input File": (0, 5, 105, 30),
@@ -58,7 +60,16 @@ class GraphUI(QMainWindow):
             buttonsLayout.addWidget(self.buttons[btnText], prop[0], prop[1])
         self.buttonsLayout.addLayout(buttonsLayout)
 
-    def createMinPathInput(self):
+
+    def _createCanvas(self):
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        self.generalLayout.addWidget(self.toolbar)
+        self.generalLayout.addWidget(self.canvas)
+
+
+    def _createMinPathInput(self):
         self.TextMinPathStart = QLineEdit(self.centralwidget)
         self.TextMinPathGoal = QLineEdit(self.centralwidget)
         font = QFont()
@@ -68,20 +79,15 @@ class GraphUI(QMainWindow):
         self.TextMinPathStart.setPlaceholderText("Start")
         self.TextMinPathGoal.setPlaceholderText("Goal")
 
-    def createAlgoOutput(self):
+
+    def _createAlgoOutput(self):
         font = QFont()
         font.setPointSize(11)
         self.AlgoOutput = QLineEdit(self.centralwidget)
         self.AlgoOutput.setText(f"Output:")
 
-    def createCanvas(self):
-        self.figure = plt.figure()
-        self.canvas = FigureCanvas(self.figure)
-        self.toolbar = NavigationToolbar(self.canvas, self)
-        self.generalLayout.addWidget(self.toolbar)
-        self.generalLayout.addWidget(self.canvas)
 
-    def setObjectsNames(self):
+    def _setObjectsNames(self):
         self.setObjectName("MainWindow")
         self.centralwidget.setObjectName("centralwidget")
         self.graphwidget.setObjectName("graphwidget")
@@ -91,7 +97,8 @@ class GraphUI(QMainWindow):
         self.TextMinPathGoal.setObjectName("TextMinPathStart")
         self.AlgoOutput.setObjectName("AlgoOutput")
 
-    def setGeometry(self):
+
+    def _setGeometry(self):
         self.centralwidget.setGeometry(0, 0, 900, 600)
         self.graphwidget.setGeometry(140, 0, 760, 590)
         self.horizontalLayoutWidget.setGeometry(QRect(5, 0, 140, 200))
@@ -99,12 +106,14 @@ class GraphUI(QMainWindow):
         self.TextMinPathGoal.setGeometry(QRect(85, 370, 50, 30))
         self.AlgoOutput.setGeometry(35, 410, 80, 30)
 
+
     def getPathFile(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         fileName, _ = QFileDialog.getOpenFileName(self, "Graph Application", path.join(getcwd(), "input.txt"), options=options)
         if fileName:
             return fileName
+
 
     def showResult(self, type, result):
         msg = QMessageBox()
@@ -117,33 +126,14 @@ class GraphUI(QMainWindow):
         msg.setStandardButtons(QMessageBox.Ok)
         msg.exec()
 
+
     def showError(self, error):
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Warning)
-        if error == "Path error":
-            msg.setWindowTitle("Path error")
-            msg.setText("Choose correct file.")
-        elif error == "File not match input type":
-            msg.setWindowTitle("File not match input type")
-            msg.setText("Choose file with correct graph input type.")
-        elif error == "Vertices not in graph":
-            msg.setWindowTitle("Vertices not in graph")
-            msg.setText("Select vertices in graph ")
-        elif error == "Minimal path error":
-            msg.setWindowTitle("The path between the vertices does not exist ")
-            msg.setText("Choose other vertices")
-        elif error == "File corrupted":
-            msg.setWindowTitle("File corrupted")
-            msg.setText("Fix file or choose another")
-        elif error == "Doesn't exist vertice":
-            msg.setWindowTitle("Doesn't exist vertice")
-            msg.setText("Input correct vertice")
-        elif error == "Uncorrect weights":
-            msg.setWindowTitle("Uncorrect weights")
-            msg.setText("Weights need be natural")
-        elif error == "Uncorrect vertice":
-            msg.setWindowTitle("Uncorrect vertice")
-            msg.setText("Vertice need be bigger than 0")
+
+        msg.setWindowTitle("Error")
+        msg.setText(str(error))
+
         msg.setStandardButtons(QMessageBox.Ok)
         msg.exec()
 
