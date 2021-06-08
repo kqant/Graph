b259a620dc30290fa95de27b48d9baa5f410d4fd
Maxim <76053782+ggodos@users.noreply.github.com>
2021-06-08 08:26:52 +0500

    Update Authors.py
    delete lodirb

--- Modules/Authors.py
+++ Modules/Authors.py
@@ -18,6 +18,5 @@ authors = \
 Рогов Дмитрий (kqant) - Back-end разработчик
 Баландин Захар (smaylninja) - Front-end разработчик
 Синельникова Илона (mouseLOL101) - Front-end разработчик
-Стахеев Дмитрий (Smirov) - Front-end разработчик
 """
 
